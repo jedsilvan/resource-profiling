18dbeb679be0d22ab466ade29ed4fd3552eda98c
Jed Silvan/Content Portal /SRPH/Engineer/Samsung Electronics <j.silvan@samsung.com>
2020-08-05 19:19:00 +0800

    initial script commit

---
+++ index.js
@@ -0,0 +1,19 @@
+// var http = require('http');
+// http.createServer(function (req, res) {
+//     res.writeHead(200, {'Content-Type': 'text/html'});
+//     res.write("Current date and time: " + modules.fetchImage('https://dn-img-page.kakao.com/download/resource?kid=cSZvNP/hyATtZmAek/QlK4PcBrTSxbHzZKk6GFxk'));
+//     res.end();
+// }).listen(8080);
+
+
+
+const modules  = require('./modules');
+const imageUrl = [
+    "https://dn-img-page.kakao.com/download/resource?kid=cSZvNP/hyATtZmAek/QlK4PcBrTSxbHzZKk6GFxk",
+    "https://dn-img-page.kakao.com/download/resource?kid=i3VME/hyzTW11Sej/nqAnIwR444EEC1NXwAQrf1",
+    "https://s.w-x.co/84impacts1.jpg"
+];
+
+for (i=0; i<3; i++) {
+    modules.fetchImage(imageUrl[i]).then(function(response) { console.log(response) });
+}
---
+++ modules.js
@@ -0,0 +1,19 @@
+const axios = require('axios');
+const sizeOf = require('buffer-image-size');
+
+exports.fetchImage = function (url) {
+    return axios.get(url, { responseType: 'arraybuffer' })
+        .then(function (response) {
+            const headers = response.headers;
+            const contentLength = headers['content-length'] * 0.001; // byte to kb
+            const imageInfo = sizeOf(response.data);
+
+            return {
+                size: parseFloat(contentLength),
+                ...imageInfo
+            }
+        })
+        .catch(function (error) {
+            console.log(error);
+        });
+};
