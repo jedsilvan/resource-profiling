3a4d431c18b496816386a909f0d1546924858f4d
Jed Silvan/Content Portal /SRPH/Engineer/Samsung Electronics <j.silvan@samsung.com>
2020-08-11 16:54:28 +0800

    add rules json

--- index.js
+++ index.js
@@ -1,28 +1,3 @@
-// var http = require('http');
-// http.createServer(function (req, res) {
-//     res.writeHead(200, {'Content-Type': 'text/html'});
-//     res.write("Current date and time: " + modules.fetchImage('https://dn-img-page.kakao.com/download/resource?kid=cSZvNP/hyATtZmAek/QlK4PcBrTSxbHzZKk6GFxk'));
-//     res.end();
-// }).listen(8080);
-
-// const modules  = require('./modules');
-// const imageUrl = [
-//     "https://dn-img-page.kakao.com/download/resource?kid=CCDwh/hyfMK8Bl85/kOyo9T8MtfielxoRvil9g1",
-//     "https://dn-img-page.kakao.com/download/resource?kid=i3VME/hyzTW11Sej/nqAnIwR444EEC1NXwAQrf1",
-//     "https://s.w-x.co/84impacts1.jpg"
-// ];
-
-// for (i=0; i<3; i++) {
-//     modules.fetchImage(imageUrl[i])
-//         .then(function(response) {
-//             console.log(response)
-//         });
-// }
-
-
-
-// 'use strict';
-
 const puppeteer = require('puppeteer');
 const chalk = require('chalk');
 const cliProgress = require('cli-progress');
@@ -160,10 +135,9 @@ const URL = 'http://localhost:3000/';
         await getImageFileSize(BODY_2, expandedAreaCollection);
         const groupedData = combineBodyAreaAndExpandedAea({ body1: bodyAreaCollection, body2: expandedAreaCollection });
 
-        // console.log("groupedData", groupedData);
+        console.log("groupedData", groupedData);
         // console.log("==============================================================");
-        // console.log("bodyAreaCollection", bodyAreaCollection);
-        console.log("imgResponse", imgResponse);
+        console.log("bodyAreaCollection", bodyAreaCollection);
         console.log("expandedAreaCollection", expandedAreaCollection);
 
 
