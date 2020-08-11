56d1ea7155593a74057187df6c177a1f1e4c4e27
Jed Silvan/Content Portal /SRPH/Engineer/Samsung Electronics <j.silvan@samsung.com>
2020-08-11 15:45:42 +0800

    script update

--- index.js
+++ index.js
@@ -21,14 +21,14 @@
 
 
 
-'use strict';
+// 'use strict';
 
 const puppeteer = require('puppeteer');
 const chalk = require('chalk');
 const cliProgress = require('cli-progress');
 
 const bar = new cliProgress.SingleBar({
-    format: chalk.cyanBright('Loading ') + '|' + chalk.cyan('{bar}') + '| {percentage}% || {value}/{total} HTTP Requests',
+    format: chalk.cyanBright('Loading ') + '|' + chalk.cyan('{bar}') + '| {percentage}% || {value}/{total} Image network requests',
     barCompleteChar: '\u2588',
     barIncompleteChar: '\u2591',
     hideCursor: true
@@ -40,7 +40,11 @@ const URL = 'http://localhost:3000/';
 
 
 (async function () {
-    const imgCollection = [];
+    const imgResponse = [];
+    const BODY_1 = "body1";
+    const BODY_2 = "body2";
+    let bodyAreaCollection = [];
+    let expandedAreaCollection = [];
     let noOfFailedRequests = 0;
     let progressTotal = 0;
 
@@ -48,8 +52,8 @@ const URL = 'http://localhost:3000/';
         bar.start(progressTotal, 0);
     }
 
-    function onRequest() {
-        bar.setTotal(progressTotal++);
+    function onRequest(request) {
+        if (request._resourceType === "image") bar.setTotal(progressTotal++);
     }
 
     function onResponse(response) {
@@ -57,14 +61,14 @@ const URL = 'http://localhost:3000/';
         const request = response._request;
 
         if (request._resourceType === "image") {
-            imgCollection.push({
-                url: request._url,
+            imgResponse.push({
+                src: request._url,
                 contentLength: convertToKb(response._headers['content-length'])
             });
-        }
 
-        if (response._status !== 200) {
-            noOfFailedRequests += 1;
+            if (response._status !== 200) {
+                noOfFailedRequests += 1;
+            }
         }
     }
 
@@ -73,8 +77,44 @@ const URL = 'http://localhost:3000/';
         return (parseInt(contentLength) * 0.001).toFixed(1);
     }
 
+    async function getImageFileSize(type, collection) {
+        if (collection && collection.length) {
+            collection.forEach((item, index) => {
+                const img = imgResponse.find(i => i.src === item.src);
+                if (!img) return;
+
+                if (type === BODY_1) {
+                    bodyAreaCollection[index].fileSize = img.contentLength;
+                } else {
+                    expandedAreaCollection[index].fileSize = img.contentLength;
+                }
+            });
+        }
+    }
+
+    function combineBodyAreaAndExpandedAea({ body1, body2 }) {
+        const groupedData = [];
+        if (!body1) return groupedData;
+        if (!body2) return body1;
+
+        const cardIds = body1.map(i => i.cardId);
+        const uniqueIds = [...new Set(cardIds)];
+
+        uniqueIds.forEach(value => {
+            const bodyArea = body1.filter(i => i.cardId === value);
+            const expandedArea = body2.filter(i => i.cardId === value);
+            groupedData.push({
+                cardId: value,
+                body1: bodyArea,
+                body2: expandedArea
+            })
+        });
+
+        return groupedData;
+    }
+
     try {
-        log(chalk.cyanBright('Launching puppeteer...'));
+        log(chalk.cyanBright('Launching chrome browser...'));
         const browser = await puppeteer.launch();
         const [page] = await browser.pages();
         page.setDefaultTimeout(0);
@@ -83,19 +123,49 @@ const URL = 'http://localhost:3000/';
         page.on('response', onResponse);
 
         log(chalk.cyanBright('Accessing ') + chalk.green(URL));
-        await page.goto(URL);
-        // await page.waitForSelector('.card-preload-container', { hidden: true });
-
+        await page.goto(URL, { waitUntil: 'networkidle0' });
+        await page.waitFor(() => !document.querySelector('.preload-container'));
 
         log(`\n${noOfFailedRequests}/${progressTotal - 1} failed requests`);
 
-        const imgDiv = await page.$$('[data-src]');
+        // remove div#intro-card from the DOM
+        await page.evaluate(() => {
+            const el = document.querySelector('#intro-card');
+            el.parentNode.removeChild(el);
+        });
 
+        bodyAreaCollection = await page.evaluate(() => Array.from(document.querySelectorAll('.bodyArea [data-src]'), element => {
+            return {
+                cardId: element.getAttribute('data-cardId'),
+                fileSize: 0,
+                height: element.getAttribute("data-height"),
+                src: element.getAttribute('data-src'),
+                templateId: element.getAttribute("data-body1"),
+                width: element.getAttribute("data-width")
+            }
+        }));
 
-        // const properties = await listHandle.getProperties();
-        log(await imgDiv[12]);
-        // console.log(await page.content());
-        // log(imgCollection);
+        expandedAreaCollection = await page.evaluate(() => Array.from(document.querySelectorAll('.expandedArea [data-src]'), element => {
+            return {
+                cardId: element.getAttribute('data-cardId'),
+                fileSize: 0,
+                height: element.getAttribute("data-height"),
+                src: element.getAttribute('data-src'),
+                templateId: element.getAttribute("data-body2"),
+                width: element.getAttribute("data-width")
+            }
+        }));
+
+        await getImageFileSize(BODY_1, bodyAreaCollection);
+        await getImageFileSize(BODY_2, expandedAreaCollection);
+        const groupedData = combineBodyAreaAndExpandedAea({ body1: bodyAreaCollection, body2: expandedAreaCollection });
+
+        // console.log("groupedData", groupedData);
+        // console.log("==============================================================");
+        // console.log("bodyAreaCollection", bodyAreaCollection);
+        console.log("imgResponse", imgResponse);
+        console.log("expandedAreaCollection", expandedAreaCollection);
+
 
         await browser.close();
     } catch (err) {
